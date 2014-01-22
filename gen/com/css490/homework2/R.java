44b5f66915c60cd16fa755072c956ab17f8f252b
TimMikeladze <tim.mikeladze@gmail.com>
2014-01-21 22:16:42 -0800

    finished tip calculator

--- gen/com/css490/homework2/R.java
+++ gen/com/css490/homework2/R.java
@@ -21,30 +21,33 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int left=0x7f020001;
-        public static final int right=0x7f020002;
+        public static final int app_background=0x7f020000;
+        public static final int bg_tile=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int left=0x7f020003;
+        public static final int right=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int edittext_bill_total=0x7f080002;
-        public static final int edittext_party_size=0x7f08000d;
-        public static final int edittext_split_total=0x7f08000f;
-        public static final int edittext_tip_percentage=0x7f080006;
-        public static final int edittext_total=0x7f08000a;
-        public static final int image_party_size_decrease=0x7f080010;
-        public static final int image_party_size_increase=0x7f080011;
-        public static final int image_tip_decrease=0x7f080005;
-        public static final int image_tip_increase=0x7f080007;
-        public static final int layout_row_1=0x7f080000;
-        public static final int layout_row_2=0x7f080003;
-        public static final int layout_row_3=0x7f080008;
-        public static final int layout_row_4=0x7f08000b;
-        public static final int textview_bill_total=0x7f080001;
-        public static final int textview_party_size=0x7f08000c;
-        public static final int textview_split_total=0x7f08000e;
-        public static final int textview_tip=0x7f080004;
-        public static final int textview_total=0x7f080009;
+        public static final int action_settings=0x7f080013;
+        public static final int edittext_bill_total=0x7f080003;
+        public static final int edittext_party_size=0x7f08000e;
+        public static final int edittext_split_total=0x7f080010;
+        public static final int edittext_tip_percentage=0x7f080007;
+        public static final int edittext_total=0x7f08000b;
+        public static final int image_party_size_decrease=0x7f080011;
+        public static final int image_party_size_increase=0x7f080012;
+        public static final int image_tip_decrease=0x7f080006;
+        public static final int image_tip_increase=0x7f080008;
+        public static final int layout_main=0x7f080000;
+        public static final int layout_row_1=0x7f080001;
+        public static final int layout_row_2=0x7f080004;
+        public static final int layout_row_3=0x7f080009;
+        public static final int layout_row_4=0x7f08000c;
+        public static final int textview_bill_total=0x7f080002;
+        public static final int textview_party_size=0x7f08000d;
+        public static final int textview_split_total=0x7f08000f;
+        public static final int textview_tip=0x7f080005;
+        public static final int textview_total=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/css490/homework2/MainActivity.java
+++ src/com/css490/homework2/MainActivity.java
@@ -1,105 +1,153 @@
 
 package com.css490.homework2;
 
+import java.text.NumberFormat;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends Activity implements TextWatcher {
-
-    private static final String DEFAULT_TIP_PERCENTAGE = "%17";
-    private static final int DEFAULT_PARTY_SIZE = 1;
-
-    private EditText billTotalTextField;
-    private EditText tipTextField;
-    private EditText totalTextField;
-    private EditText partySizeTextField;
-    private EditText splitTotalTextField;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        billTotalTextField = (EditText) findViewById(R.id.edittext_bill_total);
-        billTotalTextField.addTextChangedListener(this);
-        tipTextField = (EditText) findViewById(R.id.edittext_tip_percentage);
-        tipTextField.setText(DEFAULT_TIP_PERCENTAGE);
-        totalTextField = (EditText) findViewById(R.id.edittext_total);
-        partySizeTextField = (EditText) findViewById(R.id.edittext_party_size);
-        partySizeTextField.setText(String.valueOf(DEFAULT_PARTY_SIZE));
-        splitTotalTextField = (EditText) findViewById(R.id.edittext_split_total);
-
-    }
-
-    public void decreaseTip(View v) {
-        int percentage = Integer.parseInt(tipTextField.getText().toString().replace("%", ""));
-        percentage = percentage - 1;
-        if (percentage >= 1) {
-            tipTextField.setText("%" + percentage);
-            update();
-        }
-
-    }
-
-    public void increaseTip(View v) {
-        int percentage = Integer.parseInt(tipTextField.getText().toString().replace("%", ""));
-        percentage = percentage + 1;
-        tipTextField.setText("%" + percentage);
-        update();
-    }
-
-    public void increasePartySize(View v) {
-        int party = Integer.parseInt(partySizeTextField.getText().toString());
-        party = party + 1;
-        partySizeTextField.setText(String.valueOf(party));
-        update();
-    }
-
-    public void decreasePartySize(View v) {
-        int party = Integer.parseInt(partySizeTextField.getText().toString());
-        party = party - 1;
-        if (party >= 1) {
-            partySizeTextField.setText(String.valueOf(party));
-            update();
-        }
-    }
-
-    private void update() {
-        int percentage = Integer.parseInt(tipTextField.getText().toString().replace("%", ""));
-        int party = Integer.parseInt(partySizeTextField.getText().toString());
-        if (billTotalTextField.getText().length() > 0
-                && !billTotalTextField.getText().toString().equals(".")) {
-            double total = Double.parseDouble(billTotalTextField.getText().toString());
-
-            double billTotal = total + (percentage * total / 100);
-            double split = billTotal / party;
-
-            totalTextField.setText(String.valueOf(billTotal));
-            splitTotalTextField.setText(String.valueOf(split));
-        }
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-        update();
-
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        // TODO Auto-generated method stub
-
-    }
-
+public class MainActivity extends Activity implements TextWatcher, OnTouchListener {
+	
+	private static final String DEFAULT_TIP_PERCENTAGE = "%17";
+	private static final int DEFAULT_PARTY_SIZE = 1;
+	
+	private RelativeLayout layout;
+	private EditText billTotalTextField;
+	private EditText tipTextField;
+	private EditText totalTextField;
+	private EditText partySizeTextField;
+	private EditText splitTotalTextField;
+	
+	private String current;
+	private InputMethodManager inputMethodManager;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		layout = (RelativeLayout) findViewById(R.id.layout_main);
+		layout.setOnTouchListener(this);
+		
+		billTotalTextField = (EditText) findViewById(R.id.edittext_bill_total);
+		billTotalTextField.addTextChangedListener(this);
+		tipTextField = (EditText) findViewById(R.id.edittext_tip_percentage);
+		tipTextField.setText(DEFAULT_TIP_PERCENTAGE);
+		totalTextField = (EditText) findViewById(R.id.edittext_total);
+		partySizeTextField = (EditText) findViewById(R.id.edittext_party_size);
+		partySizeTextField.setText(String.valueOf(DEFAULT_PARTY_SIZE));
+		splitTotalTextField = (EditText) findViewById(R.id.edittext_split_total);
+		
+		inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+		
+	}
+	
+	public void decreaseTip(View v) {
+		int percentage = Integer.parseInt(tipTextField.getText()
+														.toString()
+														.replace("%", ""));
+		percentage = percentage - 1;
+		if (percentage >= 1) {
+			tipTextField.setText("%" + percentage);
+			update();
+		}
+		
+	}
+	
+	public void increaseTip(View v) {
+		int percentage = Integer.parseInt(tipTextField.getText()
+														.toString()
+														.replace("%", ""));
+		percentage = percentage + 1;
+		tipTextField.setText("%" + percentage);
+		update();
+	}
+	
+	public void increasePartySize(View v) {
+		int party = Integer.parseInt(partySizeTextField.getText()
+														.toString());
+		party = party + 1;
+		partySizeTextField.setText(String.valueOf(party));
+		update();
+	}
+	
+	public void decreasePartySize(View v) {
+		int party = Integer.parseInt(partySizeTextField.getText()
+														.toString());
+		party = party - 1;
+		if (party >= 1) {
+			partySizeTextField.setText(String.valueOf(party));
+			update();
+		}
+	}
+	
+	private void update() {
+		int percentage = Integer.parseInt(tipTextField.getText()
+														.toString()
+														.replace("%", ""));
+		int party = Integer.parseInt(partySizeTextField.getText()
+														.toString());
+		if (billTotalTextField.getText()
+								.length() > 0 && !billTotalTextField.getText()
+																	.toString()
+																	.equals(".")) {
+			double total = Double.parseDouble(billTotalTextField.getText()
+																.toString()
+																.replaceAll("[$,.]", ""));
+			double billTotal = total + (percentage * total / 100);
+			double split = billTotal / party;
+			
+			totalTextField.setText(String.valueOf(NumberFormat.getCurrencyInstance()
+																.format((billTotal / 100))));
+			splitTotalTextField.setText(String.valueOf(NumberFormat.getCurrencyInstance()
+																	.format((split / 100))));
+		}
+		
+	}
+	
+	@Override
+	public void afterTextChanged(Editable s) {
+		if (!s.toString()
+				.equals(current)) {
+			billTotalTextField.removeTextChangedListener(this);
+			
+			String cleanString = s.toString()
+									.replaceAll("[$,.]", "");
+			
+			double parsed = Double.parseDouble(cleanString);
+			String formated = NumberFormat.getCurrencyInstance()
+											.format((parsed / 100));
+			
+			current = formated;
+			billTotalTextField.setText(formated);
+			billTotalTextField.setSelection(formated.length());
+			
+			billTotalTextField.addTextChangedListener(this);
+			update();
+		}
+	}
+	
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		
+	}
+	
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		
+	}
+	
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+		return true;
+	}
 }
